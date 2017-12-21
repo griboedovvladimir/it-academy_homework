134266dc474e1e3a784d322939f2677660475034
likecoffee <likecoffee@yandex.ru>
2017-12-21 18:31:13 +0300

    Dz 1

---
+++ Дз1/index.js
@@ -0,0 +1,35 @@
+(function () {
+
+
+    printDuplicatedNumbers(1, 34, 67, 1, 5, 34, 1);
+
+    function printDuplicatedNumbers(...args) {
+        let arr = [];
+        for (let i = 0; i < args.length; i++) {
+            for (let t = 0; t < i; t++) {
+                if (args[t] === args[i]) {
+                    let counter = 0;
+                    for (let m = 0; m < args.length; m++) {
+                        if (args[t] === args[m]) {
+                            counter++
+                        }
+                    }
+                    arr.push(args[t] + ' - ' + counter);
+
+                }
+            }
+        }
+        let n = arr.length;
+        arr.sort();
+        while (n--) {
+            if (arr[n] === arr[n - 1]) {
+                arr.splice(n, 1);
+            }
+        }
+        for (let i = 0; i < arr.length; i++) {
+            console.log(arr[i]);
+        }
+    }
+
+
+}());
